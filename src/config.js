55d8b6955aaf0b53fe1c9e9e7614aa57ee2cfb85
Shashi Kant <shashi.kant@valueinnovationlabs.com>
2022-12-16 22:47:45 +0530

    s3 credential removed

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
     bucketName: 'demo-shashi',
-    region: 'ap-south-1',
-    accessKeyId: 'AKIAT63FSC2Q6TBDOOYJ',
-    secretAccessKey: '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'
+    region: 'ap-south-1'
 }
